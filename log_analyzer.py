93f496658a1f6d38cf60585fbdf490e5eccebd33
Vasyl <vstratulatus@ukr.net>
2026-09-12 19:31:43 +0100

    feat: add error message statistics

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,6 +2,8 @@ warning_count = 0
 info_count = 0
 error_count = 0
 total_lines = 0
+error_messages = {}
+
 with open("sample.log", "r") as file:
     for line in file:
         total_lines += 1
@@ -14,6 +16,11 @@ with open("sample.log", "r") as file:
 
         if "ERROR" in line:
             error_count += 1
+            error_message = line.split("ERROR", 1)[1].strip()
+            if error_message in error_messages:
+               error_messages[error_message] += 1
+            else:
+               error_messages[error_message] = 1
 
 print("Log Analysis Report")
 print("-------------------")
@@ -21,3 +28,8 @@ print("Total lines:",total_lines)
 print("INFO:", info_count)
 print("WARNING:", warning_count)
 print("ERROR:", error_count)
+
+print("Error details")
+print("-------------")
+for error, count in error_messages.items():
+    print(f"{error}: {count}")
