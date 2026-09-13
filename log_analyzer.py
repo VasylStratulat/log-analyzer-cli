64fd1252225f32c29c16f0f27b3e4f5d67ee57ea
Vasyl <vstratulatus@ukr.net>
2026-09-13 11:18:42 +0100

    feat: add IP address statistics

--- log_analyzer.py
+++ log_analyzer.py
@@ -3,7 +3,7 @@ info_count = 0
 error_count = 0
 total_lines = 0
 error_messages = {}
-
+ip_addresses = {}
 with open("sample.log", "r") as file:
     for line in file:
         total_lines += 1
@@ -17,10 +17,18 @@ with open("sample.log", "r") as file:
         if "ERROR" in line:
             error_count += 1
             error_message = line.split("ERROR", 1)[1].strip()
+            if "IP=" in error_message:
+                error_message = error_message.split("IP=", 1)[0].strip()
             if error_message in error_messages:
                error_messages[error_message] += 1
             else:
                error_messages[error_message] = 1
+        if "IP=" in line:
+            ip_address = line.split("IP=", 1)[1].strip()
+            if ip_address in ip_addresses:
+                ip_addresses[ip_address] +=1
+            else:
+                ip_addresses[ip_address] = 1
 
 print("Log Analysis Report")
 print("-------------------")
@@ -33,3 +41,11 @@ print("Error details")
 print("-------------")
 for error, count in error_messages.items():
     print(f"{error}: {count}")
+
+print("")
+print("IP statistics")
+print("-------------")
+print("Unique IP addresses:", len(ip_addresses))
+
+for ip, count in ip_addresses.items():
+    print(f"{ip}: {count}")
